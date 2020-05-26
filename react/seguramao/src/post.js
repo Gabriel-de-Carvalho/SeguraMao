4eb2bcb76f05e73f76f0a4380eea01782acce34c
Gabriel Carvalho <gabriel.fonseca@ccc.ufcg.edu.br>
2020-05-26 14:35:00 -0300

    adicionando componente de listagem e postagem

--- react/seguramao/src/post.js
+++ react/seguramao/src/post.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Postagem(props){
+  const[apoios, setApoios] = useState(props.apoio);
+
+  const apoiar = () => setApoios(apoios + 1);
+  
   return(
     <div class="postagem">
      <h1> {props.name} </h1>
      <p> {props.conteudo} </p>
-     <p> apoios: {props.apoio} </p>
-     <button type="button" name="button"> ver mais </button>
+     <p> apoios: {apoios} </p>
+     <button type="button" name="button" onClick= {apoiar}> apoiar </button>
+     <button type="button" name="button"> denunciar </button>
    </div>
 )
 };
